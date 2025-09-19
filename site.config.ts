479a2811790bf5a9fe64c5a7acb32d849505c528
Taiga <you@example.com>
2025-09-19 11:45:27 -0700

    Initial commit

---
+++ site.config.ts
@@ -0,0 +1,24 @@
+
+  export const siteConfig = {
+  title: "peepo Hunt",
+  description: "peepo Hunt",
+  organisation: "peepo",
+  tokenName: "peepo",
+  tokenShortName: "peepo",
+  // Если лендинг позиционируется как отдельный сайт для airdrop'а
+  mainSite: "",
+  twitterUrl: "https://x.com/peepobase",
+  telegramUrl: "",
+  redditUrl: "/",
+  coinmarketcupUrl: "/",
+  theme: "dark",
+  color: "#9FD919",
+  faviconIcoUrl: "https://i-p.rmcdn.net/64f4527e45849b003dc9348a/5167502/image-6fb68a45-3e59-4970-bd09-904e205d0171.png?w=115&e=webp&nll=true&cX=109&cY=0&cW=2709&cH=2817",
+  // logoUrl: "https://app.ethena.fi/custom/logo.svg",
+  // mainImageUrl: "https://app.ethena.fi/shared/ethena.svg",
+  mainImageUrl:
+    "./token-3d.png",
+  mintSumUsd: 1100000,
+  timeSec: 60 * 60,
+};
+  
